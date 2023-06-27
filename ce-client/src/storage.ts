5e37be30731cf2026a4c0c00265d83cad0341524
Carl Shen <carl.shen@databricks.com>
2023-06-26 17:05:46 -0700

    init ext

--- ce-client/src/storage.ts
+++ ce-client/src/storage.ts
@@ -1,4 +1,4 @@
-class InMemoryMapStorageMatch {
+class StorageMatch {
   location: string;
   content: string;
   embedding: string;
@@ -8,7 +8,7 @@ class InMemoryMapStorageMatch {
     this.embedding = embedding;
   };
 };
-class InMemoryMapStorageUpdate {
+class StorageUpdate {
   location: string;
   oldContent: string;
   newContent: string;
@@ -25,10 +25,10 @@ class InMemoryMapStorageUpdate {
 
 interface StorageInterface {
   addTrackedContent(location: string, content: string): Promise<void>;
-  getMatches(content: string): Promise<Array<Promise<InMemoryMapStorageMatch>>>;
-  genUpdate(oldContent: string, newContent: string, match: InMemoryMapStorageMatch): Promise<InMemoryMapStorageUpdate>;
-  genUpdates(oldContent: string, newContent: string): Promise<Array<Promise<InMemoryMapStorageUpdate>>>;
-  updateContent(update: InMemoryMapStorageUpdate): Promise<void>;
+  getMatches(content: string): Promise<Array<Promise<StorageMatch>>>;
+  genUpdate(oldContent: string, newContent: string, match: StorageMatch): Promise<StorageUpdate>;
+  genUpdates(oldContent: string, newContent: string): Promise<Array<Promise<StorageUpdate>>>;
+  updateContent(update: StorageUpdate): Promise<void>;
 };
 
 abstract class BaseStorageMixin implements StorageInterface {
@@ -48,95 +48,134 @@ abstract class BaseStorageMixin implements StorageInterface {
     return m.get(k) ?? (m.set(k, dv), dv);
   };
   abstract addTrackedContent(location: string, content: string): Promise<void>;
-  abstract getMatches(content: string): Promise<Array<Promise<InMemoryMapStorageMatch>>>;
-  abstract genUpdate(oldContent: string, newContent: string, match: InMemoryMapStorageMatch): Promise<InMemoryMapStorageUpdate>;
-  async genUpdates(oldContent: string, newContent: string): Promise<Array<Promise<InMemoryMapStorageUpdate>>> {
+  abstract getMatches(content: string): Promise<Array<Promise<StorageMatch>>>;
+  abstract genUpdate(oldContent: string, newContent: string, match: StorageMatch): Promise<StorageUpdate>;
+  async genUpdates(oldContent: string, newContent: string): Promise<Array<Promise<StorageUpdate>>> {
     return (await this.getMatches(oldContent)).map(getMatch => getMatch.then(match => this.genUpdate(oldContent, newContent, match)));
   };
-  abstract updateContent(update: InMemoryMapStorageUpdate): Promise<void>;
+  abstract updateContent(update: StorageUpdate): Promise<void>;
 }
 
-export class InMemoryMapStorage extends BaseStorageMixin implements StorageInterface {
-  private inMemoryMapStorage = { // thread-safety?
-    embeddingContentLocations: new Map<string, Map<string, string>>,
-    locationContents: new Map<string, Set<string>>,
-  };
+export class DirectEqualityInMemoryMapStorage extends BaseStorageMixin implements StorageInterface { // thread-safety?
+  private locationContents = new Map<string, Set<string>>;
+  private embeddingContentLocations = new Map<string, Map<string, string>>;
   override async addTrackedContent(location: string, content: string): Promise<void> {
     await this.awaitablePresleepForTest();
     (
-      (async () => this.computeIfAbsent(this.inMemoryMapStorage.locationContents, location, new Set).add(content))(),
+      (async () => this.computeIfAbsent(this.locationContents, location, new Set).add(content))(),
       (async () => {
         const embedding = content;
-        this.computeIfAbsent(this.inMemoryMapStorage.embeddingContentLocations, embedding, new Map).set(location, content)
+        this.computeIfAbsent(this.embeddingContentLocations, embedding, new Map).set(location, content)
       })()
     );
   };
-  override async getMatches(content: string): Promise<Array<Promise<InMemoryMapStorageMatch>>> {
+  override async getMatches(content: string): Promise<Array<Promise<StorageMatch>>> {
     const embedding = content;
-    return [...(await (async () => this.inMemoryMapStorage.embeddingContentLocations.get(embedding)?.entries())() ?? [])].map(async ([location, content]) => {
-      return new InMemoryMapStorageMatch(location, content, embedding);
+    return [...(await (async () => this.embeddingContentLocations.get(embedding)?.entries())() ?? [])].map(async ([location, content]) => {
+      return new StorageMatch(location, content, embedding);
     });
   };
-  override async genUpdate(oldContent: string, newContent: string, match: InMemoryMapStorageMatch): Promise<InMemoryMapStorageUpdate> {
+  override async genUpdate(oldContent: string, newContent: string, match: StorageMatch): Promise<StorageUpdate> {
     console.assert(match.content == oldContent);
     const newMatchContent = newContent;
     const newMatchContentEmbedding = newMatchContent;
-    return new InMemoryMapStorageUpdate(match.location, match.content, newMatchContent, match.embedding, newMatchContentEmbedding);
+    return new StorageUpdate(match.location, match.content, newMatchContent, match.embedding, newMatchContentEmbedding);
   };
-  override async updateContent(update: InMemoryMapStorageUpdate): Promise<void> {
+  override async updateContent(update: StorageUpdate): Promise<void> {
     await this.awaitablePresleepForTest();
     (
-      (async () => this.inMemoryMapStorage.locationContents.get(update.location)?.add(update.newContent))(),
-      (async () => this.inMemoryMapStorage.locationContents.get(update.location)?.delete(update.oldContent))(),
-      (async () => this.computeIfAbsent(this.inMemoryMapStorage.embeddingContentLocations, update.newContentEmbedding, new Map).set(update.location, update.newContent))(),
+      (async () => this.locationContents.get(update.location)?.add(update.newContent))(),
+      (async () => this.locationContents.get(update.location)?.delete(update.oldContent))(),
+      (async () => this.computeIfAbsent(this.embeddingContentLocations, update.newContentEmbedding, new Map).set(update.location, update.newContent))(),
       (async () => {
-        this.inMemoryMapStorage.embeddingContentLocations.get(update.oldContentEmbedding)?.delete(update.location);
-        if (this.inMemoryMapStorage.embeddingContentLocations.get(update.oldContentEmbedding)?.size == 0) {
-          this.inMemoryMapStorage.embeddingContentLocations.delete(update.oldContentEmbedding);
+        this.embeddingContentLocations.get(update.oldContentEmbedding)?.delete(update.location);
+        if (this.embeddingContentLocations.get(update.oldContentEmbedding)?.size == 0) {
+          this.embeddingContentLocations.delete(update.oldContentEmbedding);
         };
       })()
     );
   };
 };
 
-export class ExternalStorage extends BaseStorageMixin implements StorageInterface {
-  private externalStorage = { // thread-safety?
-    locationContents: new Map<string, Set<string>>, // TODO externalize, currently for test/stub, don't need to keep track content in external sources (e.g. notion), access via apis
-  };
-  override async addTrackedContent(location: string, content: string): Promise<void> {
+import { VectorOperationsApi } from '@pinecone-database/pinecone/dist/pinecone-generated-ts-fetch'; // integrate
+// import { PineconeClient } from '@pinecone-database/pinecone';
+// const pinecone = new PineconeClient();
+// const pineconeInit = {
+//   environment: 'us-west1-gcp-free',
+//   apiKey: '<pinecone key>',
+// };
+// await pinecone.init(pineconeInit);
+// const index = pinecone.Index('knoba');
+const { Configuration, OpenAIApi } = require('openai'); // lint
+const configuration = new Configuration({
+  apiKey: '<openai key>', // env
+});
+const openai = new OpenAIApi(configuration);
+export class SemanticMatchingExternalStorage extends BaseStorageMixin implements StorageInterface { // thread-safety?
+  index: VectorOperationsApi | null = null; // hacky
+  private locationContents = new Map<string, Set<string>>; // TODO externalize, currently for test/stub, don't need to keep track content in external sources (e.g. notion), access via apis
+  override async addTrackedContent(location: string, content: string): Promise<void> { // batch
     await this.awaitablePresleepForTest();
     (
-      (async () => this.computeIfAbsent(this.externalStorage.locationContents, location, new Set).add(content))(),
+      (async () => this.computeIfAbsent(this.locationContents, location, new Set).add(content))(),
       (async () => {
-        const embedding = content; // llm calculate `embedding` for `content` - cache?
-        // put `embedding` -> (`location`, `content`) in vector db
+        const embedding = (await openai.createEmbedding({ // cache
+            model: 'text-embedding-ada-002',
+            input: content,
+        }))?.data?.data[0]?.embedding; // error handling
+        const upsertRequest = {
+          vectors: [
+            {
+              id: `${location} - ${content}`, // id
+              values: embedding,
+              metadata: {
+                location: location,
+                content: content,
+              },
+            },
+          ],
+        };
+        this.index?.upsert({ upsertRequest });
       })()
     );
   };
-  override async getMatches(content: string): Promise<Array<Promise<InMemoryMapStorageMatch>>> {
+  override async getMatches(content: string): Promise<Array<Promise<StorageMatch>>> {
     return (async () => {
-      // llm calculate embedding for `content` - cache?
+      const embedding = (await openai.createEmbedding({ // cache
+        model: 'text-embedding-ada-002',
+        input: content,
+      }))?.data?.data[0]?.embedding; // error handling
       // get close, but not equivalent, matches for embedding from vector db
       // where to filter if embedding of matches and `content` of interest is not same/semantically equivalent
-      // map results to InMemoryStorageMatch
+      const queryRequest = { // pagination
+        vector: embedding,
+        topK: 10,
+        includeValues: true,
+        includeMetadata: true,
+      };
+      const queryResponse = await this.index?.query({ queryRequest });
+      // TODO map results to InMemoryStorageMatch
       return [];
     })();
   };
-  override async genUpdate(oldContent: string, newContent: string, match: InMemoryMapStorageMatch): Promise<InMemoryMapStorageUpdate> {
-    const newMatchContent = newContent; // llm generate `newMatchContent` based on semantic change (`oldContent`, `newContent`) and old `match.content`
-    const newMatchContentEmbedding = newMatchContent; // llm calculate `newMatchContentEmbedding` for `newMatchContent` - cache?
-    return new InMemoryMapStorageUpdate(match.location, match.content, newMatchContent, match.embedding, newMatchContentEmbedding);
+  override async genUpdate(oldContent: string, newContent: string, match: StorageMatch): Promise<StorageUpdate> {
+    const newMatchContent = newContent; // TODO llm generate `newMatchContent` based on semantic change (`oldContent`, `newContent`) and old `match.content`
+    const newMatchContentEmbedding = (await openai.createEmbedding({ // cache
+      model: 'text-embedding-ada-002',
+      input: newMatchContent,
+    }))?.data?.data[0]?.embedding; // error handling
+    return new StorageUpdate(match.location, match.content, newMatchContent, match.embedding, newMatchContentEmbedding);
   };
-  override async updateContent(update: InMemoryMapStorageUpdate): Promise<void> {
+  override async updateContent(update: StorageUpdate): Promise<void> {
     await this.awaitablePresleepForTest();
     (
-      (async () => this.externalStorage.locationContents.get(update.location)?.add(update.newContent))(),
-      (async () => this.externalStorage.locationContents.get(update.location)?.delete(update.oldContent))(),
+      (async () => this.locationContents.get(update.location)?.add(update.newContent))(),
+      (async () => this.locationContents.get(update.location)?.delete(update.oldContent))(),
       (async () => {
-        // put `update.newContentEmbedding` -> (`update.location`, `update.newContent`) in vector db
+        // TODO put `update.newContentEmbedding` -> (`update.location`, `update.newContent`) in vector db
       })(),
       (async () => {
-        // remove `update.oldContentEmbedding` -> `update.location` from vector db
+        // TODO remove `update.oldContentEmbedding` -> `update.location` from vector db
       })()
     );
   };
--- ce-client/test/storage.test.ts
+++ ce-client/test/storage.test.ts
@@ -1,24 +1,24 @@
-import { InMemoryMapStorage } from "../src/storage";
-// import { InMemoryMapStorage } from "./src/storage";
+import { DirectEqualityInMemoryMapStorage } from '../src/storage';
+// import { DirectEqualityInMemoryMapStorage } from './src/storage';
 
 // addTrackedContent side effect without await is async (should fail)
 // await
 (async () => {
-  const inMemoryMapStorage = new InMemoryMapStorage();
-  inMemoryMapStorage.presleepForTest = 100;
-  inMemoryMapStorage.addTrackedContent('a', 'abc');
-  console.assert((await inMemoryMapStorage.getMatches('abc')).length == 1); // should fail
+  const directEqualityInMemoryMapStorage = new DirectEqualityInMemoryMapStorage();
+  directEqualityInMemoryMapStorage.presleepForTest = 100;
+  directEqualityInMemoryMapStorage.addTrackedContent('a', 'abc');
+  console.assert((await directEqualityInMemoryMapStorage.getMatches('abc')).length == 1); // should fail
 })();
 
 // addTrackedContent side effect
 // await
 (async () => {
-  const inMemoryMapStorage = new InMemoryMapStorage();
-  inMemoryMapStorage.presleepForTest = 100;
-  await inMemoryMapStorage.addTrackedContent('a', 'abc');
-  console.assert((await inMemoryMapStorage.getMatches('abc')).length == 1);
-  const genUpdates = (await inMemoryMapStorage.genUpdates('abc', 'alphabet'));
-  await Promise.all(genUpdates.map(genUpdate => genUpdate.then(update => inMemoryMapStorage.updateContent(update))));
-  console.assert((await inMemoryMapStorage.getMatches('abc')).length == 0);
-  console.assert((await inMemoryMapStorage.getMatches('alphabet')).length == 1);
+  const directEqualityInMemoryMapStorage = new DirectEqualityInMemoryMapStorage();
+  directEqualityInMemoryMapStorage.presleepForTest = 100;
+  await directEqualityInMemoryMapStorage.addTrackedContent('a', 'abc');
+  console.assert((await directEqualityInMemoryMapStorage.getMatches('abc')).length == 1);
+  const genUpdates = (await directEqualityInMemoryMapStorage.genUpdates('abc', 'alphabet'));
+  await Promise.all(genUpdates.map(genUpdate => genUpdate.then(update => directEqualityInMemoryMapStorage.updateContent(update))));
+  console.assert((await directEqualityInMemoryMapStorage.getMatches('abc')).length == 0);
+  console.assert((await directEqualityInMemoryMapStorage.getMatches('alphabet')).length == 1);
 })();
