1c6f0b9ff1d0695849d1a35d517be1de61b7d5f6
Carl Shen <carl.shen@uwaterloo.ca>
2023-08-06 16:37:52 -0700

    Update knoba.ts

--- knoba.ts
+++ knoba.ts
@@ -239,6 +239,7 @@
                 metadata: {
                   content: uKnobaIdContent.content,
                   external_ids: JSON.stringify(Array.from(uKnobaIdContent.externalIds)),
+                },
               })),
           },
         }),
